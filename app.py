e905e364c267110f5bb0fdd14e5f5ab2a0c68bac
Saul Santos Marinho <127917213+SaulSantos1@users.noreply.github.com>
2023-03-30 14:48:24 -0300

    Update app.py
    1° Ajuste - Colocando apenas colunas de Nome, Idade e Sexo

--- app.py
+++ app.py
@@ -16,22 +16,23 @@ if opcoes == 'Sexo':
     st.title('Titanic : Sexo dos passageiros e tripulantes')
     opcaoSex = st.selectbox('Escolha o sexo:',['','Masculino','Feminino'])
     if opcaoSex == 'Masculino':
-        titanic[titanic['Sex'] == 'male' ]
-        st.write('Total = ',len(titanic[titanic['Sex'] == 'male' ]))
+        homem = titanic[titanic['Sex'] == 'male' ]
+        homem[['Name','Age','Sex']]
+        st.write('Total = ',len(homem))
     elif opcaoSex == 'Feminino': 
-        titanic[titanic['Sex'] == 'female']
-        st.write('Total = ',len(titanic[titanic['Sex'] == 'female' ]))
-
+        mulher = titanic[titanic['Sex'] == 'female']
+        mulher[['Name','Age','Sex']]
+        st.write('Total = ',len(mulher))
 if opcoes == 'Menores de 18 anos':
     st.title('Titanic : Menores de 18 anos')
     opcaoMenor = st.selectbox('Escolha a idade:',['','Entre 0 a 10 anos', 'Entre 11 a 17 anos'])
     if opcaoMenor == 'Entre 0 a 10 anos':
         crianca=titanic[titanic['Age'] <= 10]
-        crianca
+        crianca[['Name','Age','Sex']]
         st.write('Total = ',len(crianca))
     elif opcaoMenor == 'Entre 11 a 17 anos': 
         adolescente = titanic[(titanic['Age'] >= 11) & (titanic['Age'] <= 17)]
-        adolescente
+        adolescente[['Name','Age','Sex']]
         st.write('Total = ',len(adolescente))
         
 if opcoes == 'Adultos':
@@ -39,11 +40,11 @@ if opcoes == 'Adultos':
     idadeAdulto = st.selectbox('Escolha a idade:',['','Entre 18 a 35 anos','Mais de 35 anos'])
     if idadeAdulto == 'Entre 18 a 35 anos' :
         adulto = titanic[(titanic['Age'] >= 18) & (titanic['Age'] <= 35)]
-        adulto
+        adulto[['Name','Age','Sex']]
         st.write('Total = ', len(adulto))
     elif idadeAdulto == 'Mais de 35 anos':
         adultoVelho = titanic[titanic['Age'] > 35]
-        adultoVelho
+        adultoVelho[['Name','Age','Sex']]
         st.write('Total = ', len(adultoVelho))
 if opcoes == 'Não informaram idade':
     st.title('Titanic: Sem a informação da idade')
@@ -51,18 +52,18 @@ if opcoes == 'Não informaram idade':
     if semIdade:
         sIdade = titanic.replace(np.nan,'') 
         sIdade = sIdade[sIdade['Age'] == '']
-        sIdade[['Name','Sex']]
+        sIdade[['Name','Age','Sex']]
         st.write('Total = ', len(sIdade))
 if opcoes == 'Sobreviventes': 
     st.title('Titanic: Sobreviventes e Falecidos')
     opcaoSobrevivente = st.selectbox('Escolha uma opção:',['','Sobreviventes', 'Mortos'])
     if opcaoSobrevivente == 'Sobreviventes':
         sobreviventes = titanic[titanic['Survived'] == 1]
-        sobreviventes
+        sobreviventes[['Name','Age','Sex']]
         st.write('Total = ', len(sobreviventes))
     elif opcaoSobrevivente == 'Mortos':
         mortos = titanic[titanic['Survived'] == 0]
-        mortos
+        mortos[['Name','Age','Sex']]
         st.write('Total = ', len(mortos))
 
 #st.dataframe(dataframe[dataframe['Sex'] == sex])
